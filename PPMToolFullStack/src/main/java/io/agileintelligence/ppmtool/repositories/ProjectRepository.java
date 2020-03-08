9abef91430e743dbea99584540948c4fe46e26d7
Linjun Ouyang <lynjunoyang@gmail.com>
2020-03-07 16:16:19 -0800

    Implemented basic CRUD operations for projects

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/repositories/ProjectRepository.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/repositories/ProjectRepository.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Long>{
+
+    Project findByProjectIdentifier(String projectId);
+
     @Override
-    Iterable<Project> findAllById(Iterable<Long> iterable);
+    Iterable<Project> findAll();
 }
--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.agileintelligence.ppmtool.services;
 
 import io.agileintelligence.ppmtool.domain.Project;
+import io.agileintelligence.ppmtool.exceptions.ProjectIdException;
 import io.agileintelligence.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,8 +14,35 @@ public class ProjectService {
 
     public Project saveOrUpdateProject(Project project) {
 
-        // Logic (e.g. make sure it's the owner that updates)
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
     }
 
+    public Project findProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if (project == null) {
+            throw new ProjectIdException("Project does not exist");
+        }
+
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects(){
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if (project == null) {
+            throw new ProjectIdException("Cannot Project with ID '" + projectId + "'. This project does not exist");
+        }
+
+        projectRepository.delete(project);
+    }
 }
